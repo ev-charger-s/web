be60211f51309dc800d130a74cf310c022d18e07
zenedith <github@mobulum.com>
2026-04-16 16:56:51 +0200

    feat: enhance data update workflow and improve Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
+    base: '/',
+    plugins: [react(), tailwindcss()],
+    server: {
+        allowedHosts: ['localhost-vite.mobulum.xyz', 'chargers.mobulum.com', ' ev-charger-s.github.io'],
+    },
 })
